82c0172a40ec41d2d9ac878035feb5823eb0f164
thetongz <miiz_tongz@live.com>
2017-10-24 17:19:58 +0700

    base stock view

---
+++ src/components/Stock.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="stock">
+      <h1>{{getCategory()}}</h1>
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+
+  export default {
+    methods: {
+      ...mapGetters([
+        'getCategory'
+      ])
+    }
+  }
+</script>
+
+<style>
+  #stock {
+    width: 100%;
+    height: 200px;
+    margin-top: 24px;
+    border-radius: 8px;
+    border: #ffc900 solid 1px;
+    box-shadow: 0 2px 1px 0 rgba(0, 0, 0, 0.1);
+  }
+</style>
