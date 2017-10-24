dcd3e9e11f9306a80f72e179d51b0d9b5fc71eac
thetongz <miiz_tongz@live.com>
2017-10-24 16:58:41 +0700

    add index menu

---
+++ src/components/Index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="index">
+    <div v-for="menuItem in menuItems">
+      <indexMenu
+        :mainMenuTitle="menuItem.mainMenuTitle"
+        :subMenuTitles="menuItem.subMenuTitles">
+      </indexMenu>
+    </div>
+  </div>
+</template>
+
+<script>
+  import indexMenu from '@/components/IndexMenu'
+
+  export default {
+    data() {
+      return {
+        menuItems: [
+          {
+            mainMenuTitle: "ARGO",
+            subMenuTitles: ["ARGI", "FOOD"]
+          },
+          {
+            mainMenuTitle: "CONSUMP",
+            subMenuTitles: ["FASHION", "HOME", "PERSON"]
+          },
+          {
+            mainMenuTitle: "FINCIAL",
+            subMenuTitles: ["BANK", "FIN", "INSUR"]
+          },
+          {
+            mainMenuTitle: "INDUS",
+            subMenuTitles: ["AUTO", "IMM", "PAPER", "PETRO", "PKG", "STEEL"]
+          },
+          {
+            mainMenuTitle: "PROPCON",
+            subMenuTitles: ["CONMAT", "PROP", "PF&REIT", "CONS"]
+          },
+          {
+            mainMenuTitle: "RESOURC",
+            subMenuTitles: ["ENERG", "MINE"]
+          },
+          {
+            mainMenuTitle: "SERVICE",
+            subMenuTitles: ["COMM", "HELTH", "MEDIA", "PROF", "TOURISM", "TRANS"]
+          },
+          {
+            mainMenuTitle: "TECH",
+            subMenuTitles: ["ETRON", "ICT"]
+          }
+        ]
+      }
+    },
+    components: {
+      indexMenu: indexMenu
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style>
+  #index {
+    margin-left: 24px;
+    margin-top: 24px;
+    padding-bottom: 8px;
+    background-color: #fff59d;
+    width: 176px;
+    height: auto;
+  }
+</style>
---
+++ src/components/IndexMenu.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="index-menu">
+    <a class="main-menu" @click="setCategory(1)">{{mainMenuTitle}}</a>
+    <a class="sub-menu" v-for="subMenuTitle in subMenuTitles"
+        @click="setCategory(1)">{{subMenuTitle}}</a>
+  </div>
+</template>
+
+<script>
+  import { mapActions } from 'vuex'
+
+  export default {
+    props: {
+      mainMenuTitle: {
+        type: String
+      },
+      subMenuTitles: {
+        type: Array
+      }
+    },
+    methods: {
+      ...mapActions([
+        'setCategory'
+      ])
+    }
+  }
+</script>
+
+<style>
+  #index-menu {
+    padding-top: 8px;
+    margin-left: 16px;
+    text-align: left;
+  }
+
+  a.main-menu {
+    color: #373737;
+    font-size: 16px;
+    cursor: pointer;
+    display: block;
+  }
+
+  a.sub-menu {
+    margin-left: 16px;
+    color: #636363;
+    font-size: 14px;
+    cursor: pointer;
+    display: block;
+  }
+</style>
