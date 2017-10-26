fbed883d0b5095116219adca64a27f92b7a40c8f
thetongz <miiz_tongz@live.com>
2017-10-26 14:34:50 +0700

    apply modalbox in modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,8 +1,18 @@
 <template>
   <div id="modal">
-
+    <modalBox></modalBox>
   </div>
 </template>
+
+<script>
+  import modalBox from '@/components/ModalBox'
+
+  export default {
+    components: {
+      modalBox
+    }
+  }
+</script>
 
 <style>
   #modal {
@@ -11,6 +21,8 @@
     background-color: rgba(0,0,0,0.7);
     position: fixed;
     display: flex;
+    justify-content: center;
+    align-items: center;
     z-index: 1;
     left: 0;
     top: 0;
