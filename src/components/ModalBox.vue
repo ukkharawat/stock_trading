d97f9a24a8bca82e722b7bc54f7101135de050fa
thetongz <miiz_tongz@live.com>
2017-10-26 14:33:57 +0700

    add modal

---
+++ src/components/ModalBox.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="modal-box">
+    <modalBoxHeader></modalBoxHeader>
+    <modalBoxContent></modalBoxContent>
+  </div>
+</template>
+
+<script>
+  import modalBoxHeader from '@/components/ModalBoxHeader'
+  import modalBoxContent from '@/components/ModalBoxContent'
+
+  export default {
+    components: {
+      modalBoxHeader,
+      modalBoxContent
+    }
+  }
+</script>
+
+<style>
+  #modal-box {
+    background-color: #fafafa;
+    width: 50%;
+    height: auto;
+    border-radius: 8px;
+  }
+</style>
---
+++ src/components/ModalBoxHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="modal-box-header">
+    <h1 class="modal-box-text-header">Log in</h1>
+  </div>
+</template>
+
+<style>
+  #modal-box-header {
+    width: 100%;
+    height: 20%;
+    background-color : #ffc400;
+    border-top-left-radius: 8px;
+    border-top-right-radius: 8px;
+    display: flex;
+    align-items: center;
+  }
+
+  .modal-box-text-header {
+    text-align: left;
+    font-size: 40px;
+    margin: 8px 16px !important;
+  }
+</style>
