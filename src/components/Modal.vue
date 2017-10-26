6c97f99302414318584df55312a0267a656fe813
thetongz <miiz_tongz@live.com>
2017-10-26 12:19:53 +0700

    add base modal

---
+++ src/components/Modal.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="modal">
+
+  </div>
+</template>
+
+<style>
+  #modal {
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0,0,0,0.7);
+    position: fixed;
+    display: flex;
+    z-index: 1;
+    left: 0;
+    top: 0;
+  }
+</style>
