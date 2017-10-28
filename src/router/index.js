9994c6a242da553c054d4ae456b7f869b8551128
thetongz <miiz_tongz@live.com>
2017-10-28 15:45:49 +0700

    add index path

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import main from '@/components/mainPage'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   routes: [
+    {
+      path: '/',
+      component: main
+    }
   ]
 })
