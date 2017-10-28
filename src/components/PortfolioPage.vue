109b8df652e8c79115c69d911fe0ae280877a4a8
thetongz <miiz_tongz@live.com>
2017-10-28 15:50:50 +0700

    add portfolio page

---
+++ src/components/PortfolioPage.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="portfolio-page" class="row">
+    <div class="col-sm-2">
+      <index>
+        <div v-for="holdingStock in holdingStocks">
+          <indexMenu
+            :mainMenuTitle="holdingStock.title"
+            :subMenuTitles="holdingStock.stocksName">
+          </indexMenu>
+        </div>
+      </index>
+    </div>
+    <div class="col-sm-10">
+      <stock></stock>
+    </div>
+  </div>
+</template>
+
+<script>
+  import index from '@/components/Index'
+  import stock from '@/components/Stock'
+  import indexMenu from '@/components/IndexMenu'
+
+  export default {
+    data() {
+      return {
+        holdingStocks: [
+          {
+            title: "Holding Stock",
+            stocksName: ["AH"]
+          }
+        ]
+      }
+    },
+    components: {
+      index,
+      stock,
+      indexMenu
+    }
+  }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import mainPage from '@/components/MainPage'
+import portfolioPage from '@/components/PortfolioPage'
 
 Vue.use(Router)
 
@@ -10,6 +11,10 @@ export default new Router({
     {
       path: '/',
       component: mainPage
+    },
+    {
+      path: '/portfolio',
+      component: portfolioPage
     }
   ]
 })
