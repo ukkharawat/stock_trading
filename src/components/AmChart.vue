9b1dd9145b617707a3aed419df47bb5162c10775
thetongz <miiz_tongz@live.com>
2017-10-29 14:42:26 +0700

    refactor created

--- src/components/AmChart.vue
+++ src/components/AmChart.vue
@@ -4,9 +4,12 @@
 </template>
 
 <script>
-  import axios from 'axios'
   import stockService from '@/services/Stock.service'
   import stockDataSource from '@/datasources/Stock.datasource'
+
+  function sleep() {
+    return new Promise(resolve => setTimeout(resolve , 3000))
+  }
 
   export default {
     name: 'app',
@@ -15,175 +18,190 @@
         type: String
       }
     },
-    created() {
-      let dataURL = "https://raw.githubusercontent.com/wasit7/data_analytics/master/demo/data_set/"+ this.stockName +".BK.csv"
-      let stockDatas = []
-      stockService.getStockDataFromURL(dataURL)
-        .then(csvData => {
-          stockDatas = stockDataSource.getStockJsonFromCSV(csvData)
+    data() {
+      return {
+        chart: null,
+        displayData: []
+      }
+    },
+    async created() {
+      let stockData = []
+      await this.getStockData(this.stockName)
+        .then(response => {
+          stockData = response
+          this.createChart()
         })
 
-      let chart = AmCharts.makeChart("chartdiv", {
-        "type": "stock",
-        "theme": "light",
-        "glueToTheEnd": true,
+      for(let i = 0 ; i < stockData.length ; i++) {
+        this.updateChartData(stockData[i])
+        await sleep()
+      }
+    },
+    methods: {
+      updateChartData(data) {
+        this.chart.dataSets[0].dataProvider.push(data)
 
-        "dataSets": [{
-          "title": "AH",
-          "fieldMappings": [{
-            "fromField": "Open",
-            "toField": "open"
-          }, {
-            "fromField": "High",
-            "toField": "high"
-          }, {
-            "fromField": "Low",
-            "toField": "low"
-          }, {
-            "fromField": "Close",
-            "toField": "close"
-          }, {
-            "fromField": "Volume",
-            "toField": "volume"
+        this.$emit("dateChange", data["Date"])
+        this.chart.validateData()
+      },
+      async getStockData(stockName) {
+        let dataURL = "https://raw.githubusercontent.com/wasit7/data_analytics/master/demo/data_set/"+ stockName +".BK.csv"
+
+        return stockService.getStockDataFromURL(dataURL)
+          .then(csvData => {
+            return stockDataSource.getStockJsonFromCSV(csvData)
+          })
+      },
+      createChart() {
+        this.chart = AmCharts.makeChart("chartdiv", {
+          "type": "stock",
+          "theme": "light",
+          "glueToTheEnd": true,
+
+          "dataSets": [{
+            "title": "AH",
+            "fieldMappings": [{
+              "fromField": "Open",
+              "toField": "open"
+            }, {
+              "fromField": "High",
+              "toField": "high"
+            }, {
+              "fromField": "Low",
+              "toField": "low"
+            }, {
+              "fromField": "Close",
+              "toField": "close"
+            }, {
+              "fromField": "Volume",
+              "toField": "volume"
+            }],
+            "compared": false,
+            "categoryField": "Date",
+            "dataProvider": this.displayData
           }],
-          "compared": false,
-          "categoryField": "Date",
-          "dataProvider": stockDatas
-        }],
-        "dataDateFormat": "YYYY-MM-DD",
+          "dataDateFormat": "YYYY-MM-DD",
 
-        "panels": [{
-          "title": "Value",
-          "percentHeight": 70,
-
-          "stockGraphs": [{
-            "type": "candlestick",
-            "id": "g1",
-            "openField": "open",
-            "closeField": "close",
-            "highField": "high",
-            "lowField": "low",
-            "valueField": "close",
-            "lineColor": "#0f0",
-            "fillColors": "#0f0",
-            "negativeLineColor": "#db4c3c",
-            "negativeFillColors": "#db4c3c",
-            "fillAlphas": 1,
-            "comparedGraphLineThickness": 2,
-            "columnWidth": 0.7,
-            "useDataSetColors": false,
-            "balloonText": "open:<b>[[open]]</b><br>close:<b>[[close]]</b><br>low:<b>[[low]]</b><br>high:<b>[[high]]</b>",
-            "comparable": true,
-            "compareField": "close",
-            "showBalloon": true,
-            "proCandlesticks": true
-          }],
-
-          "stockLegend": {
-            "valueTextRegular": undefined,
-            "periodValueTextComparing": "[[percents.value.close]]%"
-          }
-
-        },
-
-          {
-            "title": "Volume",
-            "percentHeight": 30,
-            "marginTop": 1,
-            "showCategoryAxis": false,
+          "panels": [{
+            "title": "Value",
+            "percentHeight": 70,
 
             "stockGraphs": [{
-              "valueField": "volume",
+              "type": "candlestick",
+              "id": "g1",
               "openField": "open",
-              "type": "column",
-              "showBalloon": false,
-              "fillAlphas": 1,
-              "lineColor": "#fff",
-              "fillColors": "#fff",
+              "closeField": "close",
+              "highField": "high",
+              "lowField": "low",
+              "valueField": "close",
+              "lineColor": "#0f0",
+              "fillColors": "#0f0",
               "negativeLineColor": "#db4c3c",
               "negativeFillColors": "#db4c3c",
-              "useDataSetColors": false
+              "fillAlphas": 1,
+              "comparedGraphLineThickness": 2,
+              "columnWidth": 0.7,
+              "useDataSetColors": false,
+              "balloonText": "open:<b>[[open]]</b><br>close:<b>[[close]]</b><br>low:<b>[[low]]</b><br>high:<b>[[high]]</b>",
+              "comparable": true,
+              "compareField": "close",
+              "showBalloon": true,
+              "proCandlesticks": true
             }],
 
             "stockLegend": {
-              "markerType": "none",
-              "markerSize": 0,
-              "labelText": "",
-              "periodValueTextRegular": "[[value.close]]"
-            },
-
-            "valueAxes": [{
-              "usePrefixes": true
-            }]
-          }
-        ],
-
-        "panelsSettings": {
-          "plotAreaFillColors": "#333",
-          "plotAreaFillAlphas": 1,
-          "marginLeft": 76,
-          "marginTop": 5,
-          "marginBottom": 5
-        },
-
-        "chartScrollbarSettings": {
-          "graph": "g1",
-          "graphType": "line",
-          "usePeriod": "WW"
-        },
-
-        "valueAxesSettings": {
-          "gridColor": "#555",
-          "gridAlpha": 1,
-          "inside": false,
-          "showLastLabel": true
-        },
-
-        "balloon": {
-          "textAlign": "left",
-          "offsetY": 10
-        },
-
-        "periodSelector": {
-          "position": "bottom",
-          "periods": [
-            {
-              "period": "DD",
-              "count": 10,
-              "label": "10D"
-            }, {
-              "period": "MM",
-              "count": 1,
-              "label": "1M"
-            }, {
-              "period": "MM",
-              "count": 6,
-              "label": "6M"
-            }, {
-              "period": "YYYY",
-              "count": 1,
-              "label": "1Y"
-            },
-            {
-              "period": "MAX",
-              "label": "MAX"
+              "valueTextRegular": undefined,
+              "periodValueTextComparing": "[[percents.value.close]]%"
             }
-          ]
-        }
-      })
 
-      let index = 0
+          },
 
-      let interval = setInterval(function() {
-        if(index < stockDatas.length) {
-          chart.dataSets[0].dataProvider.push(stockDatas[index])
+            {
+              "title": "Volume",
+              "percentHeight": 30,
+              "marginTop": 1,
+              "showCategoryAxis": false,
 
-          chart.validateData()
-        } else {
-          clearInterval(interval)
-        }
-        index++
-      }, 3000)
+              "stockGraphs": [{
+                "valueField": "volume",
+                "openField": "open",
+                "type": "column",
+                "showBalloon": false,
+                "fillAlphas": 1,
+                "lineColor": "#fff",
+                "fillColors": "#fff",
+                "negativeLineColor": "#db4c3c",
+                "negativeFillColors": "#db4c3c",
+                "useDataSetColors": false
+              }],
+
+              "stockLegend": {
+                "markerType": "none",
+                "markerSize": 0,
+                "labelText": "",
+                "periodValueTextRegular": "[[value.close]]"
+              },
+
+              "valueAxes": [{
+                "usePrefixes": true
+              }]
+            }
+          ],
+
+          "panelsSettings": {
+            "plotAreaFillColors": "#333",
+            "plotAreaFillAlphas": 1,
+            "marginLeft": 76,
+            "marginTop": 5,
+            "marginBottom": 5
+          },
+
+          "chartScrollbarSettings": {
+            "graph": "g1",
+            "graphType": "line",
+            "usePeriod": "WW"
+          },
+
+          "valueAxesSettings": {
+            "gridColor": "#555",
+            "gridAlpha": 1,
+            "inside": false,
+            "showLastLabel": true
+          },
+
+          "balloon": {
+            "textAlign": "left",
+            "offsetY": 10
+          },
+
+          "periodSelector": {
+            "position": "bottom",
+            "periods": [
+              {
+                "period": "DD",
+                "count": 10,
+                "label": "10D"
+              }, {
+                "period": "MM",
+                "count": 1,
+                "label": "1M"
+              }, {
+                "period": "MM",
+                "count": 6,
+                "label": "6M"
+              }, {
+                "period": "YYYY",
+                "count": 1,
+                "label": "1Y"
+              },
+              {
+                "period": "MAX",
+                "label": "MAX"
+              }
+            ]
+          }
+        })
+      }
     }
   }
 </script>
