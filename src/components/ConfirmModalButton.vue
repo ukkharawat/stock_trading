5b7f4218013a0238fe931e9110785278053be8cd
thetongz <miiz_tongz@live.com>
2017-10-31 23:58:40 +0700

    add confirm modal

---
+++ src/components/ConfirmModalButton.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="action-button">
+    <div class="form-group">
+      <button class="btn" @click.stop.prevent="handleClick"
+              :class="buttonClass">{{message}}</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      message: {
+        type: String,
+        required: true
+      },
+      buttonClass: {
+        type: String,
+        required: true
+      }
+    },
+    methods: {
+      handleClick() {
+        this.$emit("onClickEvent", this.message)
+      }
+    }
+  }
+</script>
+
+
+<style>
+  .form-group {
+    margin-bottom: 0;
+  }
+
+  .btn {
+    border-radius: 8px;
+    width: 100%;
+    font-size: 18px;
+    background-color: #fdfdfd;
+    margin-bottom: 16px;
+  }
+
+  .no-button, .no-button:hover {
+    border: #ff0000 solid 1px;
+    color: #ff0000;
+  }
+
+  .yes-button, .yes-button:hover {
+    border: #0288d1 solid 1px;
+    color: #0288d1;
+  }
+</style>
