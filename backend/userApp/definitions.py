7cb1f2126a13be33e914b09654e246776ae5d5a6
thetongz <miiz_tongz@live.com>
2017-11-17 21:26:54 +0700

    add definition file

---
+++ backend/userApp/definitions.py
@@ -0,0 +1,4 @@
+class HttpStatus:
+    OK = 200
+    CREATED = 201
+    BAD_REQUEST = 400
--- backend/userApp/views.py
+++ backend/userApp/views.py
@@ -12,6 +12,7 @@ from django.utils.timezone import datetime
 from django.utils.formats import localize
 
 from django.contrib.auth.models import User
+from userApp.definitions import HttpStatus
 # Create your views here.
 @api_view(['POST'])
 def register(request):
@@ -29,13 +30,13 @@ def register(request):
                     "message": "Creating user succesful"
                 }
 
-                return JsonResponse(response, status = 201)
+                return JsonResponse(response, status = HttpStatus.CREATED)
             except:
                 response = {
                     "message": "User's already exist"
                 }
 
-                return JsonResponse(response, status = 400)
+                return JsonResponse(response, status = HttpStatus.BAD_REQUEST)
                 
 @api_view(['POST'])
 def authentication(request):
@@ -51,17 +52,17 @@ def authentication(request):
                         "message": "Authentication succesful"
                     }
 
-                    return JsonResponse(response, status = 200)
+                    return JsonResponse(response, status = HttpStatus.OK)
                 else:
                     response = {
                         "message": "Authentication failed"
                     }
 
-                    return JsonResponse(response, status = 400)
+                    return JsonResponse(response, status = HttpStatus.BAD_REQUEST)
                     
             except:
                 response = {
                     "message": "Authentication failed"
                 }
 
-                return JsonResponse(response, status = 400)
+                return JsonResponse(response, status = HttpStatus.BAD_REQUEST)
