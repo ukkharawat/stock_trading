5483986e1e77d0b88e5c614c843a0c0e451973ee
thetongz <miiz_tongz@live.com>
2017-11-20 02:15:04 +0700

    add user serializer for control user model

--- backend/userApp/serializers.py
+++ backend/userApp/serializers.py
@@ -1,1 +1,19 @@
 from rest_framework import serializers
+from userApp.models import UserDetail
+
+class UserDetailSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only = True)
+    username = serializers.CharField(max_length = 30)
+    cash = serializers.FloatField()
+    stepCount = serializers.IntegerField()
+
+    def create(self, validate_data):
+        return UserDetail.objects.create(**validate_data)
+
+    def update(self, instance, validate_data):
+        instance.cash = validate_data.get('cash', instance.cash)
+        instance.stepCount = validate_data.get('stepCount', instance.stepCount)
+
+        instance.save()
+
+        return instance
