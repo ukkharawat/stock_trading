73bc455b305a2eeaea6e1aa6401bd548c28117a4
thetongz <miiz_tongz@live.com>
2017-11-20 02:14:46 +0700

    add userModel for store other attribute

--- backend/userApp/models.py
+++ backend/userApp/models.py
@@ -5,3 +5,12 @@ from django.db import models
 from django.utils.timezone import datetime
 
 # Create your models here.
+
+class UserDetail(models.Model):
+    username = models.CharField(max_length = 30, unique = True, primary_key = True)
+    cash = models.FloatField(blank = False, null = False)
+    stepCount = models.IntegerField(blank = False, null = False)
+
+    class Meta:
+        ordering = ('username', )
+        default_related_name = 'userDetail'
