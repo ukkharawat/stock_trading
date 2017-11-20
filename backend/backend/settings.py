498bc28e8b54169166ba7d50f318f1c2e36277ca
thetongz <miiz_tongz@live.com>
2017-11-20 22:37:02 +0700

    add token auth

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'userApp',
     'corsheaders'
 ]
@@ -51,6 +53,15 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+   'DEFAULT_AUTHENTICATION_CLASSES': (
+       'rest_framework.authentication.TokenAuthentication',
+   ),
+   'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAdminUser'
+   ),
+}
 
 CORS_ORIGIN_ALLOW_ALL = True
 
--- backend/userApp/models.py
+++ backend/userApp/models.py
@@ -2,8 +2,11 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.utils.timezone import datetime
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+from django.conf import settings
 # Create your models here.
 
 class UserDetail(models.Model):
@@ -14,3 +17,8 @@ class UserDetail(models.Model):
     class Meta:
         ordering = ('username', )
         default_related_name = 'userDetail'
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
