5c9f3598436bd8df1f6eadb348741c221246ca9d
thetongz <miiz_tongz@live.com>
2017-11-22 14:12:54 +0700

    add userDetail admin view

--- backend/userApp/admin.py
+++ backend/userApp/admin.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from userApp.models import UserDetail
 
 # Register your models here.
+
+class UserDetailAdmin(admin.ModelAdmin):
+    listDisplay = { user.name for user in UserDetail._meta.fields }
+
+admin.site.register(UserDetail, UserDetailAdmin)
