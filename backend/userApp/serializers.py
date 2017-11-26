395bc888db1953554d13d6ecf21f312dcc9092d9
thetongz <miiz_tongz@live.com>
2017-11-26 13:18:19 +0700

    change portfolio for add , update

--- backend/userApp/serializers.py
+++ backend/userApp/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from userApp.models import UserDetail , Portfolio
+from userApp.models import UserDetail, Portfolio
 
 class UserDetailSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only = True)
@@ -19,6 +19,21 @@ class UserDetailSerializer(serializers.Serializer):
         return instance
 
 class PortfolioSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Portfolio
-        fields = '__all__'
+    id = serializers.IntegerField(read_only = True)
+    username = serializers.CharField(max_length = 30)
+    date = serializers.DateField()
+    symbol = serializers.CharField()
+    volume = serializers.IntegerField()
+    averagePrice = serializers.FloatField()
+
+    def create(self, validate_data):
+        return Portfolio.objects.create(**validate_data)
+
+    def update(self, instance, validate_data):
+        instance.date = validate_data.get('date', instance.date)
+        instance.volume = validate_data.get('volume', instance.volume)
+        instance.averagePrice = validate_data.get('averagePrice', instance.averagePrice)
+
+        instance.save()
+
+        return instance
