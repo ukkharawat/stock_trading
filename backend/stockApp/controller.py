153ccf9f6a65f7868c54c89584f907578106c084
thetongz <miiz_tongz@live.com>
2017-11-28 14:48:29 +0700

    fix indent

--- backend/stockApp/controller.py
+++ backend/stockApp/controller.py
@@ -5,7 +5,7 @@ class Controller(object):
     @staticmethod
     def createNewPortfolio(symbol, averagePrice, volume, user):
         dataDetail = Datasource.createDataDetail(symbol, averagePrice, volume)
-		portfolioDetail = Datasource.createPortfolioDetail(dataDetail)
-		newPortfolio = Datasource.createPortfolio(portfolioDetail, user)
+        portfolioDetail = Datasource.createPortfolioDetail(dataDetail)
+        newPortfolio = Datasource.createPortfolio(portfolioDetail, user)
 
         return newPortfolio
