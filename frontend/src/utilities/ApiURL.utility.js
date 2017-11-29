3ad519ad82f37bda035adec32a77f59a3a3f0b90
thetongz <miiz_tongz@live.com>
2017-11-29 14:27:33 +0700

    update APIURL

--- frontend/src/utilities/ApiURL.utility.js
+++ frontend/src/utilities/ApiURL.utility.js
@@ -5,5 +5,8 @@ export default class ApiURL {
     static loginURL = `${baseURL}/user/login`
     static logoutURL = `${baseURL}/user/logout`
     static nextDayURL = `${baseURL}/user/nextStep`
+    static getUserDetailURL = `${baseURL}/user/detail`
+    static buyStockURL = `${baseURL}/stock/buy`
+    static sellStockURL = `${baseURL}/stock/sell`
 
 }
