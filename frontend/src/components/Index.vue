1a30112ebf670fc6f6f631a70b75a4176732a064
thetongz <miiz_tongz@live.com>
2018-01-17 12:57:38 +0700

    update style

--- frontend/src/components/Index.vue
+++ frontend/src/components/Index.vue
@@ -6,11 +6,13 @@
 
 <style>
   #index {
-    margin-left: 24px;
+    margin-left: 16px;
     margin-top: 24px;
     padding-bottom: 8px;
+    border-radius: 8px;
     background-color: #fff59d;
-    width: calc(100% - 16px);
+    box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.2);
+    width: calc(100% - 24px);
     height: auto;
   }
 </style>
