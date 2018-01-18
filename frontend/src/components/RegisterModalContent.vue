45e7ca30f00c43ed714f62a64736d9e74cc2ae7f
thetongz <miiz_tongz@live.com>
2018-01-19 01:26:58 +0700

    register with api

---
+++ frontend/src/components/RegisterModalContent.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="modal-content">
+      <b-row class="justify-content-sm-center">
+        <b-col cols="8">
+          <b-form @submit="register">
+            <b-form-group id="usernameInputGroup"
+                          label="Your Username:"
+                          label-for="usernameInput">
+              <b-form-input id="usernameInput"
+                            type="text"
+                            v-model="username"
+                            required
+                            placeholder="Username">
+              </b-form-input>
+            </b-form-group>
+            <b-form-group id="passwordInputGroup"
+                          label="Your Password:"
+                          label-for="passwordInput">
+              <b-form-input id="passwordInput"
+                            type="text"
+                            v-model="password"
+                            required
+                            placeholder="Password">
+              </b-form-input>
+            </b-form-group>
+            <b-form-group id="rePasswordInputGroup"
+                          label="Type Your Password Again:"
+                          label-for="rePasswordInput">
+              <b-form-input id="rePasswordInput"
+                            type="text"
+                            v-model="repassword"
+                            required
+                            placeholder="Re-password">
+              </b-form-input>
+            </b-form-group>
+            <b-button type="submit" class="margin-top" variant="warning">Register</b-button>
+          </b-form>
+        </b-col>
+      </b-row>
+  </div>
+</template>
+
+<script>
+import userController from "@/controllers/User.controller";
+import { mapActions } from "vuex";
+
+export default {
+  data() {
+    return {
+      username: null,
+      password: null,
+      repassword: null
+    };
+  },
+  methods: {
+    ...mapActions(["closeModal"]),
+    isPasswordMatch() {
+      return this.password === this.repassword;
+    },
+    register(event) {
+      event.preventDefault();
+
+      if (this.isPasswordMatch()) {
+        userController.register(this.username, this.password).then(response => {
+          this.closeModal();
+        });
+      }
+    }
+  }
+};
+</script>
--- frontend/src/services/User.service.js
+++ frontend/src/services/User.service.js
@@ -2,44 +2,57 @@ import axios from 'axios'
 
 export default class UserService {
 
-  static async login(apiURL, userObject) {
-    let header =  {
-        'Content-Type': 'application/json'
+  static async register(apiURL, user) {
+    let header = {
+      'Content-Type': 'application/json'
     }
 
-    return axios.post(apiURL, userObject, {
-            headers: header
-        })
-        .then(response => response.data)
+    return axios.post(apiURL, user, {
+        headers: header
+      })
+      .then(response => response.data)
+  }
+
+  static async login(apiURL, user) {
+    let header = {
+      'Content-Type': 'application/json'
+    }
+
+    return axios.post(apiURL, user, {
+        headers: header
+      })
+      .then(response => response.data)
   }
 
   static async logout(apiURL, token) {
     let header = {
-        'Authorization': 'Token ' + token
+      'Authorization': 'Token ' + token
     }
-    
+
     return axios.get(apiURL, {
-            headers: header
-        })
-        .then(response => response.data)
+        headers: header
+      })
+      .then(response => response.data)
   }
 
   static async nextDay(apiURL, nextDayObject, token) {
     let header = {
-        'Authorization': 'Token ' + token
+      'Authorization': 'Token ' + token
     }
-    
+
     return axios.put(apiURL, nextDayObject, {
-            headers: header
-        })
-        .then(response => response.data)
+        headers: header
+      })
+      .then(response => response.data)
   }
 
   static async getUserDetail(apiURL, token) {
-      let header = {
-          'Authorization': 'Token ' + token
-      }
+    let header = {
+      'Authorization': 'Token ' + token
+    }
 
-      return axios.get(apiURL, {headers: header}).then(response => response.data)
+    return axios.get(apiURL, {
+      headers: header
+    }).then(response => response.data)
   }
 }
