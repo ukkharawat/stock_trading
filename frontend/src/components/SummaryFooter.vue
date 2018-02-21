17c0f8ddf24bdcef58f975e86f315bd4076ca588
thetongz <miiz_tongz@live.com>
2018-02-21 22:42:59 +0700

    add modal to footer

--- frontend/src/components/SummaryFooter.vue
+++ frontend/src/components/SummaryFooter.vue
@@ -1,19 +1,23 @@
 <template>
-  <div class="footer" v-show="isUnchangedStock">
+  <div>
+    <div class="footer" v-show="isUnchangedStock">
       <div class="row">
           <b-col cols="7" class="text-left">
-              <p class="summary" v-show="buyAmount != 0">Buy {{buyAmount}} shares,</p>
-              <p class="summary" v-show="sellAmount != 0">Sell {{sellAmount}} shares,</p>
-              <p class="summary" v-show="totalAmount != 0">Total price {{totalPrice}} baht</p>
+              <p class="summary" v-show="buyAmount != 0">Buy {{ formatNumber(buyAmount) }} shares,</p>
+              <p class="summary" v-show="sellAmount != 0">Sell {{ formatNumber(sellAmount) }} shares,</p>
+              <p class="summary" v-show="totalPrice != 0">Total price {{ totalPrice }} baht</p>
           </b-col>
           <b-col cols="5" class="text-right">
-              <p class="summary complete-trading">complete trading</p>
+              <p class="summary complete-trading" @click="openModal">complete trading</p>
           </b-col>
       </div>
+    </div>
+    <summaryModal ref="summaryModal"></summaryModal>
   </div>
 </template>
 <script>
   import { mapGetters } from 'vuex'
+  import summaryModal from '@/components/SummaryModal'
 
   export default {
     data() {
@@ -24,6 +28,9 @@
             vatRate: 1.07,
             commissionRate: 0.00157
         }
+    },
+    components: {
+        summaryModal
     },
     computed: {
         ...mapGetters([
@@ -36,8 +43,8 @@
     watch: {
         getUnchangedStocks: {
             handler(val) {
-                this.updateBuyAmount(val.filter(stock => stock.newAmount > stock.oldAmount))
-                this.updateSellAmount(val.filter(stock => stock.newAmount < stock.oldAmount))
+                this.updateBuyAmount(val.filter(stock => stock.changedAmount > 0))
+                this.updateSellAmount(val.filter(stock => stock.changedAmount < 0))
                 this.findTotalPrice(val)
             }
         },
@@ -45,15 +52,15 @@
     },
     methods: {
         updateBuyAmount(stocks) {
-            this.buyAmount = stocks.map(stock => stock.newAmount - stock.oldAmount)
+            this.buyAmount = stocks.map(stock => stock.changedAmount)
                                    .reduce((sum, e) => sum + e, 0)
         },
         updateSellAmount(stocks) {
-            this.sellAmount = stocks.map(stock => stock.oldAmount - stock.newAmount)
+            this.sellAmount = stocks.map(stock => stock.changedAmount)
                                     .reduce((sum, e) => sum + e, 0)
         },
         findTotalPrice(stocks) {
-            this.totalPrice = stocks.map(stock => (stock.newAmount - stock.oldAmount) * stock.price)
+            this.totalPrice = stocks.map(stock => stock.changedAmount * stock.price)
                                     .map(price => parseInt(price) * (1 + this.commissionRate * this.vatRate))
                                     .reduce((sum, e) => sum + e, 0)
 
@@ -64,7 +71,16 @@
                 return '0'
             price = price.toFixed(2).toString()
 
-            return price.replace(/(\d)(?=(\d{3})+\.)/g, '$1,') + " ฿"
+            return price.replace(/(\d)(?=(\d{3})+\.)/g, '$1,')
+        },
+        formatNumber(number) {
+            if(!number)
+                return '0'
+
+            return number.toString().replace(/(\d)(?=(\d{3})+)/g, '$1,')
+        },
+        openModal() {
+            this.$refs.summaryModal.openModal()
         }
     }
   }
