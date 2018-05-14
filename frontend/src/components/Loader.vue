e8bab1937e7d9c4554ecd083f38a44f4b2d391eb
thetongz <miiz_tongz@live.com>
2018-05-14 15:38:50 +0700

    add loader component

---
+++ frontend/src/components/Loader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="modal" :style="{'display': isLoadingModalOpen}">
+    <div class="loader"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isLoadingModalOpen: 'none'
+    }
+  },
+  methods: {
+    async  activateLoader() {
+      this.isLoadingModalOpen = 'block'
+      await this.sleep()
+      this.isLoadingModalOpen = 'none'
+    },
+    sleep() {
+      return new Promise(resolve => setTimeout(resolve, 750))
+    }
+  }
+}
+</script>
